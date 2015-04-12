43f12da2577310191e0d6638edb8fdd7999cbc93
WorldMan543 <baron_20.12.92@mail.ru>
2015-04-12 12:45:40 +0300

    Added SMO project

---
+++ SMO.Implementation/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer;
+
+namespace SMO.Implementation
+{
+    public class Class1
+    {
+    }
+}
